bd4df4d0df724d443cca5a3908122a2f3f91b768
trainee <trainee@260916-pc-87>
2017-05-09 15:12:18 +0530

    dispaly

---
+++ GitDemo/src/Main.java
@@ -0,0 +1,15 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		SalesData1 data=new SalesData1();
+	    displayGreeting();
+		data.display();
+		
+	}
+	public static void displayGreeting()
+	{
+		System.out.println("welcome to gitversion");
+	}
+}
---
+++ GitDemo/src/SalesData1.java
@@ -0,0 +1,13 @@
+
+public class SalesData1 {
+
+	
+	int [] data={5,5,4,8};
+	void display()
+	{
+		for(int i:data)
+		{
+			System.out.println("todo:"+data);
+		}
+	}
+}
